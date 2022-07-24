e2c9ff0b158f4d2d08473a97233a5f615b4bdb1a
Roy Gal <self.roygal@gmail.com>
2022-07-24 19:48:59 +0300

    feat(Game): Players can now see if their input is valid

--- game.py
+++ game.py
@@ -1,25 +1,41 @@
 import time
 import math
+import readline
 
 class Game:
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    ENDC = "\033[0m"
+    LINE = ">>> "
     words = ["Hello", "world", "test", "this", "app"]
+    prompt = {
+        "start": LINE,
+        "correct": GREEN + LINE + ENDC,
+        "incorrect": RED + LINE + ENDC
+    }
     last_word = str()
-    
+
     @classmethod
     def start_game(cls):
         start_time = time.time()
         words_count = len(cls.words)
+        prompt_type = "start"
         counter = 0
         
         while counter < words_count:
             cls.__prompt_words()
-            cls.__user_input()
+            cls.__user_input(prompt_type)
             # TODO: Add word highlighting for current word
-            # TODO: Set the user input as the last word he inputted when it's incorrect
             # TODO: Create somewhat of a webcrawler that gets sentences from the web and create a words list from it
             if cls.words[counter] == cls.last_word:
+                cls.last_word = str()
+                prompt_type = "correct"
                 counter += 1
+            else:
+                prompt_type = "incorrect"
+
                 
+                                
         total_time = time.time() - start_time
         total_time = "{:.3f}".format(total_time)
         
@@ -34,8 +50,8 @@ class Game:
         print(words_to_sentence)
         
     @classmethod
-    def __user_input(cls):
-        cls.last_word = input(">>> ")
+    def __user_input(cls, prompt_type):
+        cls.last_word = cls.input_with_prefill(cls.prompt[prompt_type], cls.last_word)
         cls.delete_prompt()
         cls.delete_prompt()
         
@@ -45,7 +61,15 @@ class Game:
         ERASE_LINE = '\x1b[2K'
         print(f"{CURSOR_UP_ONE}{ERASE_LINE}", end="")
         
-    
+    @staticmethod
+    def input_with_prefill(prompt, text):
+        def hook():
+            readline.insert_text(text)
+            readline.redisplay()
+        readline.set_pre_input_hook(hook)
+        result = input(prompt)
+        readline.set_pre_input_hook()
+        return result
         
 
 Game.start_game()
